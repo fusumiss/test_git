8442bdbd188fcb1797d77d04b5343eaa9e75b2c2
fusu <fusu_miss@163.com>
2020-04-26 19:47:46 +0800

    把本地工程添加到git

---
+++ git/src/com/myself/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.myself.git;
+
+/**
+ * Create By   on
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello, git...1");
+        System.out.println("hello, git...2");
+        System.out.println("hello, git...3");
+        System.out.println("hello, git...4");
+        System.out.println("hello, git...5");
+
+    }
+}
